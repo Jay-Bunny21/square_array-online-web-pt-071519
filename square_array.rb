58d7873a8b5ae30689cd5b7d96e6db13a8f88230
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-22 18:38:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,20 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each { |i| new_array << i ** 2 }
   new_array
 end
 
 
+=======
+  count = 0
+  new_array = []
+  array.each do |array|
+   return new_array << array ** 2 
+   count += 1
+ end
+ 
+ return new_array
+end
+
+>>>>>>> 1387aaca8b3a24eafe2addece1ef22e4c5dcd721
